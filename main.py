50692e6d20c424bde9f7941d8647d146d7e4cfe7
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-09-22 10:33:17 +0300

    added player rotate method

--- main.py
+++ main.py
@@ -23,11 +23,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
-        pygame.display.flip()
-        delta_time = clock.tick(60)
-        dt = delta_time / 1000
         player.draw(screen)
+        pygame.display.flip()        
+        
+        dt = clock.tick(60) / 1000        
         
 
 if __name__ == "__main__":
