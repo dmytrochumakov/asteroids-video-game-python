189462ce7fdfaaba36bd32610d36bb91777f4de2
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2024-09-25 08:45:49 +0300

    added shoot rate limmiter

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x: int, y: int):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_time = 0
 
     # in the player class
     def triangle(self):
@@ -25,6 +26,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_time -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -40,5 +42,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_time > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shoot_time = PLAYER_SHOOT_COOLDOWN
